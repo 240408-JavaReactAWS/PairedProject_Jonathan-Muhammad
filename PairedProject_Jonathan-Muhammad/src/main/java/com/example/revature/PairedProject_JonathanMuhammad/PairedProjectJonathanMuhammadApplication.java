f2efcb4ce8131905adf0ecd7b7fdc838f4fd1d62
MamoonUddin <mamoonuddin17@gmail.com>
2024-04-11 15:43:20 -0500

    Spriong Starter

---
+++ PairedProject_Jonathan-Muhammad/src/main/java/com/example/revature/PairedProject_JonathanMuhammad/PairedProjectJonathanMuhammadApplication.java
@@ -0,0 +1,13 @@
+package com.example.revature.PairedProject_JonathanMuhammad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairedProjectJonathanMuhammadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairedProjectJonathanMuhammadApplication.class, args);
+	}
+
+}
---
+++ PairedProject_Jonathan-Muhammad/src/test/java/com/example/revature/PairedProject_JonathanMuhammad/PairedProjectJonathanMuhammadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.revature.PairedProject_JonathanMuhammad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PairedProjectJonathanMuhammadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
