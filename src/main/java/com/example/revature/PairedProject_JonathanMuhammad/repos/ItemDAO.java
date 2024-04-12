0ca415cc4d048f299ecd98473f169d5567f208a7
Jonathan Pierre <JonWPierre@gmail.com>
2024-04-12 13:12:27 -0400

    Added Repository and Service Layer for items

--- src/main/java/com/example/revature/PairedProject_JonathanMuhammad/repos/ItemDAO.java
+++ src/main/java/com/example/revature/PairedProject_JonathanMuhammad/repos/ItemDAO.java
@@ -1,7 +1,10 @@
 package com.example.revature.PairedProject_JonathanMuhammad.repos;
 
+import com.example.revature.PairedProject_JonathanMuhammad.models.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface  ItemDAO  {
-
-
+@Repository
+public interface ItemDAO extends JpaRepository<Item, Integer> {
 }
+
--- src/main/java/com/example/revature/PairedProject_JonathanMuhammad/services/ItemService.java
+++ src/main/java/com/example/revature/PairedProject_JonathanMuhammad/services/ItemService.java
@@ -1,4 +1,50 @@
 package com.example.revature.PairedProject_JonathanMuhammad.services;
 
+import com.example.revature.PairedProject_JonathanMuhammad.models.Item;
+import com.example.revature.PairedProject_JonathanMuhammad.repos.ItemDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ItemService {
+
+    @Autowired
+    ItemDAO itemDAO;
+
+    public ItemService() {
+    }
+
+    public ItemService(ItemDAO itemDAO) {
+        this.itemDAO = itemDAO;
+    }
+
+    public Optional<Item> getItemById(int id){
+        return itemDAO.findById(id);
+    }
+
+    public List<Item> getAllItems(){
+        return itemDAO.findAll();
+    }
+
+    public Item addItem(Item item){
+        return (Item)itemDAO.save(item);
+    }
+
+    public Item updateItem(Item item){
+        if(itemDAO.findById(item.getItemId())!=null){
+            return (Item)itemDAO.save(item);
+        }
+        return null;
+    }
+
+    public int deleteItemById(int id){
+        if(itemDAO.findById(id)!=null){
+            itemDAO.deleteById(id);
+            return -1;
+        }
+        return 0;
+    }
 }
