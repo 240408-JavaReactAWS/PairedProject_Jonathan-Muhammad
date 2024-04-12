4e561cd04fdc5e94683aed92f4d07c1762ff1121
Jonathan Pierre <JonWPierre@gmail.com>
2024-04-12 13:19:45 -0400

    Added Controller Layer for items

--- src/main/java/com/example/revature/PairedProject_JonathanMuhammad/PairedProjectJonathanMuhammadApplication.java
+++ src/main/java/com/example/revature/PairedProject_JonathanMuhammad/PairedProjectJonathanMuhammadApplication.java
@@ -2,8 +2,14 @@ package com.example.revature.PairedProject_JonathanMuhammad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.example.revature.PairedProject_JonathanMuhammad") //scan com revature package for spring beans
+@EntityScan("com.example.revature.PairedProject_JonathanMuhammad.models")
+@EnableJpaRepositories("com.example.revature.PairedProject_JonathanMuhammad.repos")
 public class PairedProjectJonathanMuhammadApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/revature/PairedProject_JonathanMuhammad/controllers/ItemController.java
+++ src/main/java/com/example/revature/PairedProject_JonathanMuhammad/controllers/ItemController.java
@@ -1,4 +1,72 @@
 package com.example.revature.PairedProject_JonathanMuhammad.controllers;
 
+import com.example.revature.PairedProject_JonathanMuhammad.models.Item;
+import com.example.revature.PairedProject_JonathanMuhammad.services.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequestMapping("/items")
 public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    public ItemController() {
+    }
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Item> addItem(@RequestBody Item item){
+        Item savedItem = itemService.addItem(item);
+        if(savedItem != null){
+            return new ResponseEntity<>(savedItem,OK);
+        }
+        return new ResponseEntity<>(NOT_ACCEPTABLE);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Item>> getAllItems(){
+        return new ResponseEntity<>(itemService.getAllItems(),OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable("id") int id) {
+        Optional foundItem = itemService.getItemById(id);
+        if (foundItem != null) {
+            return new ResponseEntity<>((Item)foundItem.get(), OK);
+        }
+        return new ResponseEntity<>(NOT_FOUND);
+    }
+
+    @PutMapping
+    public ResponseEntity<Item> updateItem(@RequestBody Item item) {
+        Optional foundItem = itemService.getItemById(item.getItemId());
+        if (foundItem != null) {
+            Item updatedItem = itemService.updateItem(item);
+            return new ResponseEntity<>(updatedItem, OK);
+        }
+        return new ResponseEntity<>(NOT_FOUND);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Integer> deleteItemById(@PathVariable("id") int id) {
+        Optional foundItem = itemService.getItemById(id);
+        if (foundItem != null) {
+            Item item=(Item)foundItem.get();
+            int deletedItem = itemService.deleteItemById(item.getItemId());
+            return new ResponseEntity<>(deletedItem, OK);
+        }
+        return new ResponseEntity<>(NOT_FOUND);
+    }
 }
